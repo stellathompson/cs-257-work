8785cf85c2a4b164b793b4d6066b8209521a334f
Stella Thompson <stellathompson@Stellas-MacBook-Air-4.local>
2024-01-17 11:51:39 -0600

    fixing merge conflict

--- sqllab/createtable.sql
+++ sqllab/createtable.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS earthquakes;
 CREATE TABLE earthquakes (
-  quaketime time with time zone,
+  quaketime datetime,
   latitude real,
   longitude real,
   quakedepth real,
   mag real,
-  place text,
+  place text
 );
