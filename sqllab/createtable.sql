bb87d89ce240bb0de1d4a4c7b03f07eb9355a080
Stella Thompson <stellathompson@Stellas-MacBook-Air-4.local>
2024-01-17 09:35:29 -0600

    SQL file added

---
+++ sqllab/createtable.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS earthquakes;
+CREATE TABLE earthquakes (
+  quaketime time with time zone,
+  latitude real,
+  longitude real,
+  quakedepth real,
+  mag real,
+  place text,
+);
