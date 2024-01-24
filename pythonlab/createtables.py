d2daf35278b42ab022e6077b5b79079ab65f7caf
Stella Thompson <stellathompson@Stellas-MacBook-Air-4.local>
2024-01-24 09:22:08 -0600

    print statements to debug

--- pythonlab/createtables.py
+++ pythonlab/createtables.py
@@ -1,5 +1,24 @@
 import psycopg2
 
+
+
+def test_connection():
+
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="thompsons",
+        user="thompsons",
+        password="paper876stars")
+
+    if conn is not None:
+        print( "Connection Worked!" )
+    else:
+        print( "Problem with Connection" )
+
+    return None
+
+
 def create_tables():
 
     conn = psycopg2.connect(
