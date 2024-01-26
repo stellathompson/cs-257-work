3eae9017cada004e8743c6ab1e049d960be2ecdc
Stella Thompson <stellathompson@Stellas-MacBook-Air-4.local>
2024-01-26 09:10:25 -0600

    query two

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -35,4 +35,40 @@ def test_query_one():
 
     conn.commit()
 
+def test_query_two():
+
+    conn = psycopg2.connect(
+        host="localhost",
+        port=5432,
+        database="thompsons",
+        user="thompsons",
+        password="paper876stars")
+
+    cur = conn.cursor()
+
+    sql = " SELECT * FROM cities ORDER BY population DESC "
+    
+    cur.execute( sql )
+
+    # fetchone() returns one row that matches your quer
+    row = cur.fetchone()
+
+    if row == None:
+        print("Something went wrong...")
+    else:
+        print(row[0] + " has a population of " + row[2] ", the biggest in the dataset.")
+
+
+    # Note: We could access individual items in the row
+    # That is, row[0] would be the name column in the previous example
+    #   ... and row[1] would be the abb column
+
+    #IMPORTANT: This function doesn't actually change the database
+    #If we are trying to change the database ...
+    # ... for example, creating a table
+    #Then we need the following command to finalize our changes
+
+    conn.commit()
+
 test_query_one()
+test_query_two()
