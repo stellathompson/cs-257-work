9815eaf53d4f505d2751df89be16b0ef6402c655
Stella Thompson <stellathompson@Stellas-MacBook-Air-4.local>
2024-02-19 09:24:01 -0600

    adding random number generator

---
+++ csslab/my-work/number_generator.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import render_template
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    welcome_message = "Welcome to my random number generator! Click the button to get a new random number."
+    return render_template("homepage.html", someText = welcome_message)
+
+def numberGenerator():
+    randomNumber = random.randint(0, 10000)
+    return str(randomNumber)
+
+if __name__ == '__main__':
+    my_port = 5135
+    app.run(host='0.0.0.0', port = my_port) 
---
+++ csslab/my-work/static/example.js
@@ -0,0 +1,3 @@
+function randomNumber() {
+  window.location.href = "/";
+}
